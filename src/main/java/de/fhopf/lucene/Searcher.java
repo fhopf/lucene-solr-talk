0c6c1219549ee72d8795adf93cd4676d2b314290
Florian Hopf <hopf@synyx.de>
2012-06-13 09:58:54 +0200

    simple searching from the command line

--- src/main/java/de/fhopf/lucene/Searcher.java
+++ src/main/java/de/fhopf/lucene/Searcher.java
@@ -8,10 +8,12 @@ import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -59,10 +61,25 @@ public class Searcher {
     }
 
     public List<Document> search(String query) throws ParseException {
-        QueryParser queryParser = new QueryParser(Version.LUCENE_36, "content", new GermanAnalyzer(Version.LUCENE_36));
+        QueryParser queryParser = new QueryParser(Version.LUCENE_36, "all", new GermanAnalyzer(Version.LUCENE_36));
         Query actualQuery = queryParser.parse(query);
         logger.info("Searching for {}", query);
         return search(actualQuery);
 
+    }
+
+    public static void main(String [] args) throws IOException, ParseException {
+        if (args.length != 2) {
+            System.out.println(String.format("Usage: java %s <indexdir> <query>", Searcher.class.getCanonicalName()));
+            System.exit(0);
+        }
+
+        Directory dir = FSDirectory.open(new File(args[0]));
+        Searcher searcher = new Searcher(dir);
+        List<Document> docs = searcher.search(args[1]);
+        System.out.println(String.format("Found %d results", docs.size()));
+        for (Document doc: docs) {
+            System.out.println(String.format("%s", doc.get("title")));
+        }
     }
 }
