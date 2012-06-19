11f09e711c26e0bd878d810652677ad616e5f6e5
Florian Hopf <hopf@synyx.de>
2012-06-19 12:39:58 +0200

    using Junit 4 Test case

--- solr/src/test/java/de/fhopf/lucenesolrtalk/solr/BasicConfigurationTest.java
+++ solr/src/test/java/de/fhopf/lucenesolrtalk/solr/BasicConfigurationTest.java
@@ -19,65 +19,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class BasicConfigurationTest extends AbstractSolrTestCase {
+public class BasicConfigurationTest extends SolrTestCaseJ4 {
 
-    private EmbeddedSolrServer server;
-
-//    @BeforeClass
-//    public static void initCore() throws Exception {
-//        SolrTestCaseJ4.initCore("solr/conf/solrconfig.xml", "solr/conf/schema.xml");
-//    }
-@Override
-public String getSchemaFile() {
-    return "solr/conf/schema.xml";
-}
-
-    @Override
-    public String getSolrConfigFile() {
-        return "solr/conf/solrconfig.xml";
-    }
-
-    @Before
-    public void initSolrServer() throws Exception {
-        super.setUp();
-        server = new EmbeddedSolrServer(h.getCoreContainer(), "");
+    @BeforeClass
+    public static void initCore() throws Exception {
+        SolrTestCaseJ4.initCore("solr/conf/solrconfig.xml", "solr/conf/schema.xml");
     }
 
     @Test
-    public void queryIsNotFound() throws SolrServerException {
-//                assertQ("test query on empty index",
-//            req("qlkciyopsbgzyvkylsjhchghjrdf")
-//            ,"//result[@numFound='0']"
-//            );
-
-
-
-        SolrParams params = new SolrQuery("text that is not found");
-        QueryResponse response = server.query(params);
-        assertEquals(0L, response.getResults().getNumFound());
-
-        server.getCoreContainer().shutdown();
+    public void noResultInEmptyIndex() throws SolrServerException {
+        assertQ("test query on empty index",
+                req("text that is not found")
+                , "//result[@numFound='0']"
+        );
     }
 
-    //@Test(expected = SolrServerException.class)
+    @Test
     public void pathIsMandatory() throws SolrServerException, IOException {
-        SolrInputDocument document = new SolrInputDocument();
-        document.addField("title", "the title");
-
-        server.add(document);
-        server.commit();
+        assertFailedU(adoc("title", "the title"));
     }
 
-    //@Test
+    @Test
     public void simpleDocumentIsIndexedAndFound() throws SolrServerException, IOException {
-       server.commit();
-    }
+        assertU(adoc("path", "/tmp/foo", "title", "the title"));
+        assertU(commit());
 
-    @After
-    public void releaseSolr() throws Exception {
-        super.tearDown();
-        server.shutdown();
-
+        assertQ("added document found",
+                req("title:title")
+                , "//result[@numFound='1']"
+        );
     }
 
 }
