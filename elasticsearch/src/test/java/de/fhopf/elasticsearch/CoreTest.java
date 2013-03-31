3eecaf110a3259bb162761045b5eda681ed2de38
Florian Hopf <mail@florian-hopf.de>
2013-03-31 16:02:50 +0200

    added simple test that indexes and searches tasks

--- elasticsearch/src/test/java/de/fhopf/elasticsearch/CoreTest.java
+++ elasticsearch/src/test/java/de/fhopf/elasticsearch/CoreTest.java
@@ -5,25 +5,35 @@ import com.google.common.io.Resources;
 import de.fhopf.elasticsearch.test.ElasticsearchTestNode;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.*;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.node.NodeBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.facet.FacetBuilders;
 import org.elasticsearch.search.facet.terms.TermsFacet;
 import org.elasticsearch.search.facet.terms.TermsFacetBuilder;
 import org.junit.Rule;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Execute some core indexing and searching.
  */
 public class CoreTest {
+
     public static final String INDEX = "myindex";
     public static final String TYPE = "document";
-
     @Rule
     public ElasticsearchTestNode testNode = new ElasticsearchTestNode();
 
@@ -56,11 +66,11 @@ public class CoreTest {
 
         // one facet returned
         assertThat(actionGet.facets().facets().size()).isEqualTo(1);
-        
+
         // default analyzer
         TermsFacet facet = actionGet.facets().facet("myTerms");
         assertThat(facet.getTotalCount()).isEqualTo(7);
-        
+
         // Result: seems to be using only lowercasing so let's see if we can switch to german
 //  "facets" : {
 //    "myTerms" : {
@@ -89,7 +99,7 @@ public class CoreTest {
 //      } ]
 //    }        
     }
-    
+
     @Test
     public void germanTextIsStemmed() throws IOException {
         String text = "Texte";
@@ -97,13 +107,13 @@ public class CoreTest {
 
         // index needs to be created before mapping can be added
         testNode.getClient().admin().indices().prepareCreate(INDEX).execute().actionGet();
-        
+
         String mapping = Resources.toString(getClass().getResource("/test-mapping.json"), Charsets.UTF_8);
         testNode.getClient().admin().indices().preparePutMapping(INDEX).setType(TYPE).setSource(mapping).execute().actionGet();
-        
+
         index("1", "test", text);
         index("2", "test", expected);
-        
+
         // only one term cause it is stemmed
         List<String> terms = getTerms(INDEX, "test");
         assertThat(terms.size()).isEqualTo(1);
@@ -113,23 +123,64 @@ public class CoreTest {
         assertThat(response.getHits().getTotalHits()).isEqualTo(2);
     }
 
+    @Test
+    public void indexAndSearchTalk() throws IOException {
+        Client esClient = testNode.getClient();
+
+        // use put mapping api to have speaker value keyword-analyzed
+        esClient.admin().indices().prepareCreate("bedcon").addMapping("talk", XContentFactory.jsonBuilder()
+                .startObject().startObject("talk")
+                    .startObject("properties")
+                        .startObject("speaker")
+                            .field("type", "string").field("analyzer", "keyword")
+                        .endObject()
+                    .endObject()
+                .endObject().endObject()).execute().actionGet();
+        
+        XContentBuilder sourceBuilder =
+                XContentFactory.jsonBuilder()
+                .startObject()
+                .field("title",
+                "Verteiltes Suchen mit Elasticsearch")
+                .field("date",
+                new Date())
+                .array("speaker",
+                new String[]{"Dr. Halil-Cem Gürsoy"})
+                .endObject();
+
+        IndexRequest request =
+                new IndexRequest("bedcon", "talk")
+                .source(sourceBuilder).refresh(true);
+
+        esClient.index(request).actionGet();
+        
+        SearchResponse response = esClient.prepareSearch("bedcon")
+                .addFacet(FacetBuilders.termsFacet("speaker").field("speaker"))
+                .setQuery(QueryBuilders.queryString("elasticsearch"))
+                .execute().actionGet();
+        assertEquals(1, response.getHits().getTotalHits());
+        SearchHit hit = response.getHits().getAt(0);
+        assertEquals("Verteiltes Suchen mit Elasticsearch", hit.getSource().get("title"));
+        TermsFacet facet = response.facets().facet(TermsFacet.class, "speaker");
+        assertEquals(1, facet.entries().size());
+        
+    }
+
     private void index(String id, String field, String value) throws IOException {
         testNode.getClient().prepareIndex(INDEX, TYPE, id)
                 .setSource(jsonBuilder().startObject().field(field, value).endObject()).setRefresh(true)
                 .execute()
                 .actionGet();
     }
-    
+
     private List<String> getTerms(String index, String field) {
         TermsFacetBuilder builder = new TermsFacetBuilder("myTerms").field(field).allTerms(true);
         SearchResponse actionGet = testNode.getClient().prepareSearch(index).addFacet(builder).setQuery(QueryBuilders.matchAllQuery()).setExplain(true).execute().actionGet();
         TermsFacet facet = actionGet.facets().facet("myTerms");
         List<String> result = new ArrayList<>();
-        for (TermsFacet.Entry entry: facet.entries()) {
+        for (TermsFacet.Entry entry : facet.entries()) {
             result.add(entry.getTerm());
         }
         return result;
     }
-    
-    
 }
